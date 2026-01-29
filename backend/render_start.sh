3fb677c14883684ecbb4fb70ddd14fa341317ec8
engsoftcris <cristiano.tobias40@gmail.com>
2026-01-29 19:53:17 -0300

    Removendo migração de admin e adicionando createsuperuser no script

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -9,11 +9,15 @@ echo "--- [CHECK 2]: CONSIGO LER O MANAGE.PY? ---"
 ls -l manage.py
 
 echo "--- [CHECK 3]: TESTANDO CONEXÃO COM O BANCO (SEM RODAR MIGRATE) ---"
-# O check apenas tenta carregar as configurações, sem alterar o banco.
 python manage.py check
 
 echo "--- PASSO 1: RODANDO MIGRATE ---"
 python manage.py migrate --noinput 2>&1
+
+# --- NOVO: CRIANDO O SUPERUSER SEM INTERAÇÃO ---
+echo "--- PASSO 1.5: CRIANDO SUPERUSER ---"
+# O '|| true' serve para o script não travar caso o usuário já exista
+python manage.py createsuperuser --noinput || echo "Superuser já existe ou erro na criação."
 
 echo "--- PASSO 2: SUBINDO GUNICORN ---"
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
