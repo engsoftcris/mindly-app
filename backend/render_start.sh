ff3d12b17bdb4b1a2cf0ee226bdde3ba20b8a53c
engsoftcris <cristiano.tobias40@gmail.com>
2026-01-29 18:47:47 -0300

    fix: corrige permissões e quebra de linha do script de start

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+echo "--- PASSO 1: SCRIPT INICIOU ---"
 python manage.py migrate --noinput
+echo "--- PASSO 2: MIGRATE TERMINOU ---"
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
