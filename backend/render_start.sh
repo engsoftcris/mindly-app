f5cae158c0135f2c6e3138f6a38cd8e2b827e323
engsoftcris <cristiano.tobias40@gmail.com>
2026-01-29 18:04:17 -0300

    Deploy with superuser

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-set -e
-
-echo "--> Rodando Migrations..."
 python manage.py migrate --noinput
-
-echo "--> Iniciando Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
